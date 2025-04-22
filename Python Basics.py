51a742ed68bb49fc75a765e73b2249d1c9010b97
Arie k. <67206811+a972k@users.noreply.github.com>
2025-04-22 11:36:37 +0300

    class work assignment

---
+++ Python Basics.py
@@ -0,0 +1,15 @@
+#assigment 1 
+sentence = input("enter a sentence: ") # input function
+length = len(sentence) # len function to calculate length of string
+print("the lengeth of the sentence is ", length) # print function 
+#assigment 2
+favorite_color = input("What is your favorite color? ")
+#favorite_color = it is a very siple name
+print("Your favorite color is", favorite_color)
+my_age = input("What is your age? ")
+#my_age = it is a very siple name
+print("Your age is", my_age)
+#my_age = it is a very siple name
+favorite_animal = input("What is your favorite animal? ")
+#favorite_animal = it is a very siple name
+print("Your favorite animal is", favorite_animal)
