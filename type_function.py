488effe87c3bc753bcac5883d0a2b2a66e439ce7
Arie k. <67206811+a972k@users.noreply.github.com>
2025-04-23 18:19:45 +0300

    type function added

---
+++ type_function.py
@@ -0,0 +1,6 @@
+num = 10
+string = "Hello"
+boll = True
+print("The vlue of num is", num, "and the type of num is", type(num))
+print("The vlue of string is", string, "and the type of string is", type(string))
+print("The vlue of boll is", boll, "and the type of boll is", type(boll))
