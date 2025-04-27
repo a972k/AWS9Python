a07d67cccb0392542aef480a829ac0c3ea05814d
Arie k. <67206811+a972k@users.noreply.github.com>
2025-04-27 10:52:56 +0300

    Reverse a String
    added file

---
+++ Reverseastring.py
@@ -0,0 +1,7 @@
+def revers_string(string):
+   
+    return string[::-1]
+srring = str(input("Enter a string: "))
+reversed_string = revers_string(string)
+print("Reversed string is:" { reversed_sring } )
+  
